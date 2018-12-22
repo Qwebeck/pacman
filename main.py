2d4c01cb582551cfa15dd07006566c48e6be501c
Qwebeck <qwebeck5@gmail.com>
2018-12-22 15:51:59 -0500

    Finish first part

---
+++ main.py
@@ -0,0 +1,121 @@
+import pygame as pg
+import sys
+from os import path
+from settings import *
+from sprites import *
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        pg.key.set_repeat(500, 100)
+        self.load_data()
+
+
+    def load_data(self):
+        self.game_folder = path.dirname(__file__)
+        self.img_folder = path.join(self.game_folder, 'images')
+        self.player_image = pg.transform.scale(pg.image.load(path.join(self.img_folder, PACMAN_IMAGE[1])),
+                                        (TILESIZE, TILESIZE))
+
+
+        self.map_data = []
+        with open(path.join(self.game_folder, 'map.txt'), 'rt') as f:
+            for line in f:
+                self.map_data.append(line)
+
+
+    def new(self):
+        # initialize all variables and do all the setup for a new game
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        self.coins = pg.sprite.Group()
+
+        map_len = len(self.map_data[0])
+        for row, tiles in enumerate(self.map_data):
+            for col, tile in enumerate(tiles):
+                col += int((GRIDWIDTH-map_len)/2)
+                # row += int(GRIDHEIGHT/2)
+                row += 5
+                if tile == '1':
+                    Wall(self, col, row)
+                if tile == '.':
+                    Coins(self,col,row)
+                if tile == 'P':
+                    self.player = Player(self, col, row)
+                if tile == 'T':
+                    Wall(self, col, row)
+                col -= int((GRIDWIDTH-map_len)/2)
+                row -= 5
+    def run(self):
+        # game loop - set self.playing = False to end the game
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
+
+    def quit(self):
+        pg.quit()
+        sys.exit()
+
+    def update(self):
+        # update portion of the game loop
+        self.all_sprites.update()
+
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+    def events(self):
+        # catch all events here
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+                if event.key == pg.K_LEFT:
+                    self.player_image = pg.transform.rotate(self.player.image, 90)
+                    self.swap(0)
+                if event.key == pg.K_RIGHT:
+                    # self.player.keys = 1
+                    self.swap(1)
+                if event.key == pg.K_UP:
+                    # self.player.keys = 3
+                    self.swap(3)
+                if event.key == pg.K_DOWN:
+                    # self.player.keys = 2
+                    self.swap(2)
+
+    def swap(self, dir):
+        if self.player.previous_key == -1 :
+            self.player.keys = dir
+            self.player.previous_key = dir
+        else:
+            self.player.keys,self.player.previous_key = dir,self.player.keys
+
+    def show_start_screen(self):
+        pass
+
+    def show_go_screen(self):
+        pass
+
+# create the game object
+g = Game()
+g.show_start_screen()
+while True:
+    g.new()
+    g.run()
+g.show_go_screen()
---
+++ sprites.py
@@ -0,0 +1,112 @@
+import pygame as pg
+import sys
+from os import path
+from settings import *
+vec = pg.math.Vector2
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.shadow = pg.sprite.Group()
+        self.image = self.game.player_image
+        self.rect = self.image.get_rect()
+        self.keys = -1
+        self.previous_key = -1
+        self.vel = vec(0, 0)
+        self.pos = vec(x, y) * TILESIZE
+        self.index =0
+
+    def animation(self):
+        if self.index == 2:
+            self.index = 0
+        # self.image = pg.transform.scale(pg.image.load(path.join(self.game.img_folder, PACMAN_IMAGE[self.index])),
+        #                                 (TILESIZE, TILESIZE))
+        self.index += 1
+
+    def get_keys(self, key): #check for two keys
+        self.vel = vec(0, 0)
+        if key == 0: #left
+            self.vel.x = -PLAYER_SPEED
+        elif key == 1: #right
+            self.vel.x = PLAYER_SPEED
+        elif key == 2:#down
+            self.vel.y = PLAYER_SPEED
+        elif key == 3:#up
+            self.vel.y = -PLAYER_SPEED
+
+    def collide_with_walls(self, dir):
+        hits = pg.sprite.spritecollide(self, self.game.walls, False)
+        if dir == 'x':
+            if hits:
+
+                if self.vel.x > 0:
+                    self.pos.x = hits[0].rect.left - self.rect.width
+                elif self.vel.x < 0:
+                    self.pos.x = hits[0].rect.right
+                self.vel.x = 0
+                self.rect.x = self.pos.x
+                return True
+        if dir == 'y':
+            if hits:
+                if self.vel.y > 0:
+                    self.pos.y = hits[0].rect.top - self.rect.width
+                elif self.vel.y < 0:
+                    self.pos.y = hits[0].rect.bottom
+                self.vel.y = 0
+                self.rect.y = self.pos.y
+                return True
+
+    def move(self):
+        # if self.previous_key != -1:
+        #     self.keys = self.previous_key
+        # self.get_keys()
+        self.pos += self.vel * self.game.dt
+        self.rect.x = self.pos.x
+        coll = self.collide_with_walls('x')
+        self.rect.y = self.pos.y
+        if not coll:
+            coll = self.collide_with_walls('y')
+        return coll
+
+    def update(self):
+        self.animation()
+        self.get_keys(self.keys)
+        collision = self.move()
+        if collision:
+            self.get_keys(self.previous_key)
+            sec = self.move()
+        else:
+            self.previous_key = self.keys
+
+
+
+class Wall(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.walls
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+
+class Coins(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.coins
+        pg.sprite.Sprite.__init__(self, self.groups)
+        game_folder = path.dirname(__file__)
+        img_folder = path.join(game_folder, 'images')
+        self.image = pg.transform.scale(pg.image.load(path.join(img_folder, 'coin.png')),
+                                        (TILESIZE, TILESIZE))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y =y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+
